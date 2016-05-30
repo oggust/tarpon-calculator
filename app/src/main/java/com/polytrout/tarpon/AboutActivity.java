dc9930cca6326a73ccc954cc94ae44783687a58c
Björn Augustsson <oggust@gmail.com>
2016-05-30 23:30:40 +0200

    * Add (clickable!) TBRC logo and blurb.

--- app/src/main/java/com/polytrout/tarpon/AboutActivity.java
+++ app/src/main/java/com/polytrout/tarpon/AboutActivity.java
@@ -1,13 +1,28 @@
 package com.polytrout.tarpon;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_about);
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ImageView img = (ImageView) findViewById(R.id.tbrc_logo);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.tarponresearch.com/"));
+                startActivity(intent);
+            }
+        });
+    }
 }
